4293ef0d2f47063ee884c6120f1df1240f4de8cd
Andy Carter <andy@andrewcarter.co.uk>
2020-09-03 15:24:49 +0100

    Update to PoC to update the user app_metadata from within the Poc, and not using a rule for this purpose. Removal of token checking from the Auth0 redirect rule as this is not needed with the user being authenticated again within the PoC.

--- Net Core/Controllers/HomeController.cs
+++ Net Core/Controllers/HomeController.cs
@@ -1,14 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
-using System.Text;
+using System.Net;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
-using Microsoft.IdentityModel.Protocols;
-using Microsoft.IdentityModel.Tokens;
 using Net_Core.Models;
 using Newtonsoft.Json;
 using RestSharp;
@@ -21,8 +18,6 @@ namespace NetCore.Controllers
         /// <summary>
         /// Configuration
         /// </summary>
-        private readonly string _tokenValidationIssuer;
-        private readonly string _tokenValidationAudience;
         private readonly string _clientId;
         private readonly string _clientSecret;
         private readonly string _domain;
@@ -30,56 +25,29 @@ namespace NetCore.Controllers
         private readonly string _scopes;
         private readonly string _redirectUri;
         private readonly string _customClaimNamespace;
-        private readonly string _tokenValidationSigningKey;
-
-        private static IConfiguration _configuration;
+        private readonly string _managementApiAudience;
+        private readonly string _managementApiDomain;
 
         public HomeController(IConfiguration config)
         {
-            _configuration = config;
-            _tokenValidationIssuer = _configuration["Auth0:TokenValidation:Issuer"];
-            _tokenValidationAudience = _configuration["auth0:TokenValidation:Audience"];
-            _clientId = _configuration["auth0:ClientId"];
-            _clientSecret = _configuration["auth0:ClientSecret"];
-            _domain = _configuration["auth0:Domain"];
-            _audience = _configuration["auth0:Audience"];
-            _scopes = _configuration["auth0:ClientScopes"];
-            _redirectUri = _configuration["auth0:RedirectUri"];
-            _customClaimNamespace = _configuration["auth0:CustomClaimNamespace"];
-            _tokenValidationSigningKey = _configuration["auth0:TokenValidation:SigningKey"];
+            var configuration = config;
+            _clientId = configuration["auth0:ClientId"];
+            _clientSecret = configuration["auth0:ClientSecret"];
+            _domain = configuration["auth0:Domain"];
+            _audience = configuration["auth0:Audience"];
+            _scopes = configuration["auth0:ClientScopes"];
+            _redirectUri = configuration["auth0:RedirectUri"];
+            _customClaimNamespace = configuration["auth0:CustomClaimNamespace"];
+            _managementApiAudience = configuration["auth0:ManagementApiAudience"];
+            _managementApiDomain = configuration["auth0:ManagementApiDomain"];
         }
-        public IActionResult Index(string token, string state)
+
+
+        public IActionResult Index(string state)
         {
-            // If the token or state is not present, return an error
-            if (string.IsNullOrEmpty(token) || string.IsNullOrEmpty(state))
+            // If the state parameter from the Auth0 redirect is not present, return an error
+            if (string.IsNullOrEmpty(state))
             {
-                return RedirectToAction("Error");
-            }
-
-            // Validate the token sent from the Auth0 redirect rule
-            TokenValidationParameters validationParameters =
-                new TokenValidationParameters
-                {
-                    ValidIssuer = _tokenValidationIssuer,
-                    ValidAudiences = new[] { _tokenValidationAudience },
-                    IssuerSigningKeys = new List<SecurityKey> { new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_tokenValidationSigningKey)) }
-                };
-
-            SecurityToken validatedToken;
-            var handler = new JwtSecurityTokenHandler();
-            try
-            {
-                handler.ValidateToken(token, validationParameters, out validatedToken);
-            }
-            catch (Exception)
-            {
-                // The token could not be validated, return an error
-                return RedirectToAction("Error");
-            }
-
-            if (validatedToken == null)
-            {
-                // The token could not be validated, return an error
                 return RedirectToAction("Error");
             }
 
@@ -106,27 +74,7 @@ namespace NetCore.Controllers
                 return View("Error");
             }
             // Read the ID token sent and extract the SMS number from the token
-            var handler = new JwtSecurityTokenHandler();
-            JwtSecurityToken token;
-            try
-            {
-                token = handler.ReadJwtToken(idToken);
-            }
-            catch (Exception)
-            {
-                // The token could not be read, return an error
-                Log.Error("Could not read id token {0}", idToken);
-                return RedirectToAction("Error");
-            }
-
-            if (token.Claims.All(c => c.Type.ToLower() != $"{_customClaimNamespace}smsnumber"))
-            {
-                // The phone number claim is not present
-                Log.Error("A claim of smsnumber is not present in the user's id token {0}", idToken);
-                return RedirectToAction("Error");
-            }
-
-            var smsNumber = token.Claims.FirstOrDefault(c => c.Type.ToLower() == $"{_customClaimNamespace}smsnumber")?.Value;
+            var smsNumber = GetClaimValueFromToken(idToken, $"{_customClaimNamespace}smsnumber");
 
             // Store the state and mfa token
             HttpContext.Session.SetString("mfaToken", mfaToken);
@@ -145,7 +93,7 @@ namespace NetCore.Controllers
                 return RedirectToAction("Error");
             }
 
-            var enrollResponse = JsonConvert.DeserializeObject<EnrollResponse>(response.Content);//serializer.Deserialize<EnrollResponse>(response.Content);
+            var enrollResponse = JsonConvert.DeserializeObject<EnrollResponse>(response.Content);
             var oobCode = enrollResponse.OobCode;
 
             // Store the oob_code returned
@@ -182,6 +130,29 @@ namespace NetCore.Controllers
                 return RedirectToAction("ProcessEnrollment");
             }
 
+            // Update the user in Auth0 to say the process is complete
+            // Request a token to use with the Auth0 management API
+            var managementApiTokenResponse = GetManagementApiToken();
+            if (managementApiTokenResponse == null)
+            {
+                return RedirectToAction("Error");
+            }
+            // Get the subject claim from the MFA token so we have the user id
+            var userId = GetClaimValueFromToken(mfaToken, "sub");
+
+            var apiClient = new RestClient($"{_managementApiAudience}users/{userId}");
+            var apiRequest = new RestRequest(Method.PATCH);
+            apiRequest.AddHeader("authorization", $"Bearer {managementApiTokenResponse.AccessToken}");
+            apiRequest.AddHeader("content-type", "application/json");
+            apiRequest.AddParameter("application/json", "{\"app_metadata\": {\"mfa_enrollment_complete\": \"true\"}}", ParameterType.RequestBody);
+            IRestResponse apiResponse = apiClient.Execute(apiRequest);
+
+            if (apiResponse.ResponseStatus == ResponseStatus.Error)
+            {
+                Log.Error("Could not update user's app_metadata in Auth0");
+                return RedirectToAction("Error");
+            }
+
             // Get the state from session
             var state = HttpContext.Session.GetString("state");
 
@@ -195,10 +166,63 @@ namespace NetCore.Controllers
         }
 
 
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        /// <summary>
+        /// Requests an access token for use with the Auth0 Management API
+        /// </summary>
+        /// <returns>A ManagementApiTokenResponse object containing the access token</returns>
+        private ManagementApiTokenResponse GetManagementApiToken()
+        {
+            var managementTokenClient = new RestClient($"https://{_managementApiDomain}/oauth/token");
+            var managementTokenRequest = new RestRequest(Method.POST);
+            managementTokenRequest.AddHeader("content-type", "application/x-www-form-urlencoded");
+            managementTokenRequest.AddParameter("application/x-www-form-urlencoded", $"grant_type=client_credentials&client_id={_clientId}&client_secret={_clientSecret}&audience={_managementApiAudience}&scope=update:users_app_metadata", ParameterType.RequestBody);
+            IRestResponse managementTokenResponse = managementTokenClient.Execute(managementTokenRequest);
+            if (managementTokenResponse.StatusCode != HttpStatusCode.OK)
+            {
+                // The token was not issued, return an error
+                Log.Error("Could not get a management API token {0}", managementTokenResponse.ErrorMessage);
+                return null;
+            }
+
+            ManagementApiTokenResponse managementApiTokenResponse =
+                JsonConvert.DeserializeObject<ManagementApiTokenResponse>(managementTokenResponse.Content);
+            return managementApiTokenResponse;
+        }
+
+        private string GetClaimValueFromToken(string token, string claim)
+        {
+            var handler = new JwtSecurityTokenHandler();
+            JwtSecurityToken jwtToken;
+            try
+            {
+                jwtToken = handler.ReadJwtToken(token);
+            }
+            catch (Exception)
+            {
+                // The token could not be read, return an error
+                Log.Error("Could not read token {0}", token);
+                return null;
+            }
+
+            if (jwtToken.Claims.All(c => c.Type.ToLower() != "sub"))
+            {
+                // The phone number claim is not present
+                Log.Error($"A claim of {claim} is not present in the token {0}", token);
+                return null;
+            }
+
+            var value = jwtToken.Claims.FirstOrDefault(c => c.Type.ToLower() == claim)?.Value;
+
+            return value;
+        }
+
     }
+
 }
